b6bffe7bebb82a443883962ee3163ebfa0ab6152
Ricardo199 <ricardo1529@gmail.com>
2024-09-23 22:00:24 -0400

    Add new route for '/Ben-Kenobi'

--- my-app/routes/users.js
+++ my-app/routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/Ben-Kenobi', function(req, res, next) {
+  res.send('Of course i know him, he is me');
+});
+
 module.exports = router;
